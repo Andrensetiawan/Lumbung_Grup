fd49e51fe8a12a979800b80e28c1ff2e60dbfd8b
andrensetiawan <andrensetiawan@gmail.com>
2026-05-21 18:52:50 +0700

    Change video container to 21:9 ultrawide landscape ratio

--- components/AboutPageClient.tsx
+++ components/AboutPageClient.tsx
@@ -93,7 +93,7 @@ export default function AboutPageClient({ content }: Props) {
                 </p>
               </div>
             </div>
-            <div className="relative hidden aspect-video overflow-hidden rounded-[2rem] shadow-2xl lg:block">
+            <div className="relative hidden overflow-hidden rounded-[2rem] shadow-2xl lg:block" style={{ aspectRatio: "21/9" }}>
               <video
                 src="/Optical sorting.mp4"
                 className="h-full w-full object-cover animate-fade-in"
